fc190d19d9cfc7038c59ad2961d089da7866c5d7
lhrvt <you@example.com>
2024-03-12 22:49:01 +0100

    plus de couleur c'est naze

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import * as THREE from './build/three.module.js'
 import {GLTFLoader} from './build/GLTFLoader.js'
 
-const Btn_Vert = document.getElementById('btn_vert');
-const Btn_Jaune = document.getElementById('btn_jaune');
-const Btn_Bleu = document.getElementById('btn_bleu');
-const Btn_Neutral = document.getElementById('btn_none');
+
 let actual_color;
 var color_change = false;
 
@@ -12,32 +9,6 @@ const jaune_pale = new THREE.Color(0xf5c722);
 const vert = new THREE.Color(0x6be03d);
 const bleu = new THREE.Color(0x6078e0);
 
-Btn_Jaune.addEventListener('click', function(event) {
-  color_change = true;
-  actual_color = jaune_pale
-  boxMesh.material.color.set(actual_color)
-  
-});
-Btn_Vert.addEventListener('click', function(event) {
-  color_change = true;
-  actual_color = vert
-  boxMesh.material.color.set(actual_color)
- 
-  
-});
-Btn_Bleu.addEventListener('click', function(event) {
-  color_change = true;
-  actual_color = bleu
-  boxMesh.material.color.set(bleu)
-  
-});
-Btn_Neutral.addEventListener('click', function(event) {
-  
-  color_change = false;
-  actual_color = 'white'
-  boxMesh.material.color.set(actual_color)
-  
-});
 
 
 
@@ -95,6 +66,7 @@ raycaster.setFromCamera(coords, camera);
     }
     //console.log(selectedObject.name + "was clicked!");
     playanimation(selectedObject.name)
+    console.log(selectedObject)
   }
 
 }
@@ -106,6 +78,11 @@ const scene = new THREE.Scene()
  const loader = new GLTFLoader()
 
  let root;
+ let TEXT;
+ const toonmat = new THREE.MeshToonMaterial({
+
+  color: 'white-smoke'
+})
 const animation_chargee = []
 animation_chargee.push("CLI_placard", "CLI_frigo", "CLI_congelo","CLI_plante")
  loader.load('asset/cuisinne_urp.glb', function(gltf ){
@@ -119,18 +96,21 @@ animation_chargee.push("CLI_placard", "CLI_frigo", "CLI_congelo","CLI_plante")
     placard   = gltf.animations[2] ; 
     plante   = gltf.animations[3] ;// second animation
 
-    placard.loop = false;
-    mixer.clipAction(placard).play()
-
+    
+    //mixer.clipAction(placard).play()
+    TEXT = gltf.scene.getObjectByName('Text_rm');
+    
+    TEXT.material = toonmat
+    TEXT.material.wireframe = true
     root = gltf .scene;
     root.position.set(-0.2,0,2)
     root.rotation.y = -Math.PI / 2
 
     scene.add(root);
-  
+    
  }, function(xhr){
     console.log(xhr.loaded/xhr.total * 100 + "% loaded")
-}, function(error){
+  }, function(error){
      console.log('An error')
  } 
  );
@@ -153,7 +133,7 @@ const material = new THREE.MeshToonMaterial({
 
     color: 'white'
 })
-const boxMesh = new THREE.Mesh(geometry, material)
+const boxMesh = new THREE.Mesh(geometry, toonmat)
 boxMesh.position.set(-0.7,2.5,1)
 boxMesh.scale.set(0.5,0.5,0.5)
 scene.add(boxMesh)
@@ -168,8 +148,7 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100);
 camera.position.set(0,0.7,4);
-scene.add(camera)- document.querySelector('p').offsetHeight - document.querySelector('.button-container')
-
+scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
     canvas : canvas 
@@ -179,7 +158,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width , sizes.height )
 
 
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, ))
+renderer.setPixelRatio(window.devicePixelRatio)
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; // 
 renderer.gammaOutput= true
@@ -196,7 +175,7 @@ function animate(){
     light.rotation.y = time;
     root.position.set(Math.sin(time ) *0.1 - 0.2, -0.4, 2)
     //console.log(time_light)
-    
+    TEXT.scale.y= Math.cos(time );
     renderer.render(scene,camera)
     
     
